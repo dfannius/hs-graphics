d90dc6ae21eb8b06cd4921a3bd184c110fd64a75
Dan Schmidt <dfan@dfan.org>
2013-11-18 14:21:52 -0500

    Move to Double to avoid fromIntegral

--- src/fingering.hs
+++ src/fingering.hs
@@ -5,14 +5,11 @@
 import Control.Monad
 import Data.Char
 import qualified Graphics.Rendering.Cairo as C
-import qualified Graphics.Rendering.Cairo.Matrix as M
 
 data KeyColor = White | Black
 
 type PC = Int     -- pitch class
 type Finger = Int -- fingering
-
-keyWidth w = (fromIntegral w) / 12.0
 
 keyColors = [White, Black, White, Black, White, White, Black, White, Black, White, Black, White]
 fingerColors = [(0.3, 0.0, 0.7),
@@ -38,7 +35,7 @@ fingerings = map parseFingering
       "1 23 1 2 34 ",
       "1 2 31 2 34 "]
 
-drawFinger :: Int -> Int -> PC -> Int -> Finger -> C.Render ()
+drawFinger :: Double -> Double -> PC -> Int -> Finger -> C.Render ()
 drawFinger w h pc lineNo finger = do
   C.save
 
@@ -49,7 +46,7 @@ drawFinger w h pc lineNo finger = do
 
   C.setSourceRGB 1 1 1
   C.selectFontFace "Arial" C.FontSlantNormal C.FontWeightBold
-  C.setFontSize ((fromIntegral w) / 25.0)
+  C.setFontSize (w / 25.0)
   C.setLineWidth 1.0
   (C.TextExtents txb tyb tw th txa tya) <- C.textExtents text
   C.moveTo (x - txa/2) (y + th/2)
@@ -58,28 +55,29 @@ drawFinger w h pc lineNo finger = do
 
   C.restore
       where (red,green,blue) = fingerColors !! (finger - 1)
-            x = (keyWidth w) * (0.5 + fromIntegral pc)
-            y = (keyWidth w) * (0.5 + fromIntegral lineNo)
-            r = (keyWidth w) * 0.4
+            keyWidth = w / 12.0
+            x = keyWidth * (0.5 + fromIntegral pc)
+            y = keyWidth * (0.5 + fromIntegral lineNo)
+            r = keyWidth * 0.4
             text = show finger
 
-drawScaleFingering :: Int -> Int -> Int -> [(PC, Finger)] -> C.Render()
+drawScaleFingering :: Double -> Double -> Int -> [(PC, Finger)] -> C.Render()
 drawScaleFingering w h lineNo fs = do
   C.save
   forM_ fs $ \(pc, f) -> drawFinger w h pc lineNo f
   C.restore
     
-renderKey :: Int -> Int -> PC -> KeyColor -> C.Render ()
+renderKey :: Double -> Double -> PC -> KeyColor -> C.Render ()
 renderKey w h idx color = do
   C.save
-  C.rectangle ((fromIntegral idx) * (keyWidth w)) 0 (keyWidth w) (fromIntegral h)
+  C.rectangle ((fromIntegral idx) * (keyWidth w)) 0 (keyWidth w) h
   C.setFillRule C.FillRuleWinding
   case color of
     White -> C.stroke
     Black -> C.fill
   C.restore
 
-renderFingering :: Int -> Int -> C.Render ()
+renderFingering :: Double -> Double -> C.Render ()
 renderFingering w h = do
   C.save
   forM_ (zip [0..] keyColors) $ \(i, c) -> renderKey w h i c
@@ -90,7 +88,7 @@ renderFingering w h = do
 main :: IO ()
 main = do
   C.withImageSurface C.FormatARGB32 width height $ \surf -> do
-    C.renderWith surf $ renderFingering width height
+    C.renderWith surf $ renderFingering (fromIntegral width) (fromIntegral height)
     C.surfaceWriteToPNG surf "fingering.png"
     where width  = 400
           height = 400
